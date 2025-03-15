4ffebd15c08934aed9132720acabea265a6d443b
Muneeb Shah <codewithmuneeb7@gmail.com>
2025-03-15 23:39:42 +0500

    Started Trees

---
+++ Others/YT/Tree Implementation.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+	public: 
+		int data;
+		Node *left;
+		Node *right;
+	
+	Node(int data)
+	{
+		this->data = data;
+		this->left = NULL;
+		this->right = NULL;
+	}
+	
+	void insertNode(int data)
+	{
+		
+	}
+	
+	void preOrderTraversal()
+	{
+		
+	}
+	
+	void printTree()
+	{
+		
+	}
+};
+int main()
+{
+	
+}
