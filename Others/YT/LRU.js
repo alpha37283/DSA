6e9403264c57cd0133694bb5b2ac02fef1a91eea
Muneeb Shah <codewithmuneeb7@gmail.com>
2025-03-27 23:36:05 +0500

    LRU dry run

--- Others/YT/LRU.js
+++ Others/YT/LRU.js
@@ -1,142 +1,91 @@
 class LRU {
     constructor(capacity){
         this.capacity = capacity;
-        this.map = new Map(); // for storing the LL address o(1) ops
-        this.head = head;
-        this.tail = tail;
-        this.length = 0; // track length
-
+        this.map = new Map(); // HashMap for O(1) lookup
+        this.head = null; // Initialize head
+        this.tail = null; // Initialize tail
+        this.length = 0; // Track length
     }
-    #removeNode(node){
 
-        if(!node)
-        {
-            return ;
-        }
-        // if there is a prev of node
-        if(node.prev)
-        {
+    #removeNode(node){
+        if (!node) return;
+
+        if (node.prev) {
             node.prev.next = node.next; 
         }
-        
 
-        // if there is a next of node
-        if(node.next)
-        {
+        if (node.next) {
             node.next.prev = node.prev;
         }
 
-        // if the node is itself a head
-        if(node === this.head)
-        {
+        if (node === this.head) {
             this.head = node.next;
         }
 
-        // if the node is itself a tail
-        if(node = this.tail)
-        {
+        if (node === this.tail) {
             this.tail = node.prev;
         }
-
     }
-    get(key){
 
+    get(key){
+        if (!this.map.has(key)) {
+            return -1;  // Return -1 if key is not found
+        }
+
+        const node = this.map.get(key);
+
+        this.#removeNode(node);
+
+        node.prev = null;
+        node.next = this.head;
+
+        if (this.head !== null) {
+            this.head.prev = node;  
+        }
+
+        this.head = node;
+
+        if (!this.tail) {
+            this.tail = node;
+        }
+
+        return node.value;
     }
 
     put(key, value){
-        // Case: if the capacity is full
-        if(this.length === this.capacity)
-        {
-            if(!(this.map.has(key))) // and the key is not already exist
-            {
-                this.#removeNode(this.tail);
-                this.length -= 1;
-                // remove the tail
-                // and add a new node at the end
-
-            }
-        }
-
-
-
-        // Case: if the key already exist
-        if(this.map.has(key))
-        {
+        if (this.map.has(key)) {
             this.#removeNode(this.map.get(key));
+        } else if (this.length === this.capacity) {
+            this.map.delete(this.tail.key);
+            this.#removeNode(this.tail);
+            this.length -= 1;
         }
 
-        // creating a node
-        const node = {
-            next : this.head,
-            prev : null,
-            value,
-        }
-        this.head = node;
+        const node = { key, value, next: this.head, prev: null };
 
-        if(this.tail === null)
-        {
-            this.tail = head;
-        }
-
-        this.map.set(key, node);
-
-        if(this.head != null)
-        {
+        if (this.head !== null) {
             this.head.prev = node;
         }
 
+        this.head = node;
+
+        if (this.tail === null) {
+            this.tail = node;
+        }
+
+        this.map.set(key, node);
         this.length += 1;
     }
 
-    debug()
-    {
+    debug() {
         let current = this.head;
         const arr = [];
 
-        while(current != null)
-        {
-            arr.push(current);
+        while (current !== null) {
+            arr.push(`(${current.key}: ${current.value})`);
             current = current.next;
         }
+
+        console.log("LRU Cache:", arr.join(" -> "));
     }
-
-   
 }
-
-
-// solution
-
-/**
- 
-we can use an array to store the hash(key , value)
-but in that case lookup will be O(n) where n is if larger in case of Worst case, we will have problem
-
-we can use LL
-- in that case the operation to 
-get(key) will be O(n)
-put(key, value) will also be of O(n)
-
-steps:
-# put(key, value)
-
-if LL is empty, simply create a new node
-if not empty, place the new Node at the end 
-if full return
-if already exist 
-    - remove the existing node from LL and Hash
-    - create a new node at the end
-
-
-# get(key)
-
-if(!exist)
-- return not exist
-else 
-- nd = new Node();
-// tail would be LRU node 
-
-nd -> next = head;
-tail -> next = head; 
-temp = head->next->next === NULL;
-
- */
