d972838581285ba6df45dcc159e7e6dd6a6f2c65
Muneeb Shah <codewithmuneeb7@gmail.com>
2025-04-03 21:14:02 +0500

    TScheduler errors exist

--- Others/YT/TaskScheduler.js
+++ Others/YT/TaskScheduler.js
@@ -1,8 +1,64 @@
 class TaskScheduler{
-    addTask(){
+    constructor(concurrency)
+    {
+        this.concurrency = concurrency;
+        this.runningTasks = 0;
+        this.waitingQueue = [];
+    }
+
+    getNextTask()
+    {
+        if(this.concurrency > this.runningTasks && length(this.waitingQueue) > 0)
+        {
+            const nextTask = this.waitingQueue.shift();
+            nextTask();
+        }
+        else
+        {
+            console.log('No elements in Q or concurrency limit.');
+        }
 
     }
+    
+    addTask(task){
+       return new Promise((resolve, reject)=>{
+            async function __taskRunner()
+            {
+                try{
+                    const result = await task();
+                    console.log(result);
+                    resolve(result);
+                    this.runningTasks += 1;
+                }
+                catch(err){
+                    console.log(err);
+                    reject(err);
+                }
+                finally
+                {
+                    this.runningTask -= 1;
+                    this.getNextTask();
+                }
+            }
+
+            if(this.concurrency > this.runningTasks)
+            {
+                __taskRunner.call(this);
+            }
+            else
+            {
+                this.waitingQueue.push(__taskRunner.bind(this));
+            }
+       })
+    }
 }
+
+const scheduler = new TaskScheduler(2);
+
+scheduler.addTask(new Promise((res) => setTimeout(() => res('Task 1'), 5 * 1000)));
+scheduler.addTask(new Promise((res) => setTimeout(() => res('Task 2'), 2 * 1000)));
+scheduler.addTask(new Promise((res) => setTimeout(() => res('Task 3'), 1 * 1000)));
+scheduler.addTask(new Promise((res) => setTimeout(() => res('Task 4'), 5 * 1000)));
 
 
 // only maxium of LIMIT at can run at a time 
