520a920dee70377712a30481fde85cebbad6950e
alpha37283 <codewithmuneeb7@gmail.com>
2025-08-03 12:28:51 +0500

    container with most water

---
+++ Arrays/containerWithMostWater.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int ans = 0;
+
+        int l = 0;
+        int r = height.size() - 1; 
+
+        while(l < r)
+        {
+            int w = r - l;
+            int h = min(height[l], height[r]);
+            int currentWater = w * h; 
+            ans = max(ans, currentWater);
+
+            if(height[l] < height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+            
+        }
+
+        return ans; 
+    }
+
+};
