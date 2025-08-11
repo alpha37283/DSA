e2fc88eb535143b6c18a036c5057d0d8b65489ea
alpha37283 <codewithmuneeb7@gmail.com>
2025-08-11 21:01:28 +0500

    Q4

---
+++ Arrays/SlidingWindows/LC1343.cpp
@@ -0,0 +1,37 @@
+
+
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int n = arr.size();
+        int i = 0;              
+        int j = 0;  
+        int sum = 0;
+        int count = 0;
+
+        
+        while (j < k) {
+            sum += arr[j];
+            j++;
+        }
+
+    
+        if (sum / k >= threshold) {
+            count++;
+        }
+
+    
+        while (j < n) {
+            sum += arr[j];   
+            sum -= arr[i];   
+            i++;
+            j++;
+
+            if (sum / k >= threshold) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
