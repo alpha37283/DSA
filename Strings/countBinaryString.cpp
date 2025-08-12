fd7ded7a5a6f76c38532d8c7b6be014fc393e18a
alpha37283 <codewithmuneeb7@gmail.com>
2025-08-12 20:38:15 +0500

    idk

---
+++ Strings/countBinaryString.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int prev = 0;
+        int curr = 1; 
+        int res = 0;
+
+        for(int i = 1; i<s.size(); i++)
+        {
+            if(s[i] == s[i - 1])
+            {
+                curr += 1; 
+            }
+            else
+            {
+                res += min(prev, curr);
+                prev = curr;
+                curr = 1; 
+            }
+
+        }
+
+        res += min(prev,curr);
+
+        return res;
+    }
+};
